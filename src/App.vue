b425454344bf27fa2dbf07f05e61dd8d25fa6081
Abdulaziz <abizlancer@gmail.com>
2023-07-13 16:59:09 +0500

    feat: add webfonts and some scss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <router-view />
+  <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit incidunt tenetur fugiat mollitia reprehenderit! Saepe autem, facere expedita provident dolorum, molestiae repudiandae pariatur delectus, voluptatibus nisi repellat praesentium magnam harum?</h1>
+  <!-- <router-view /> -->
 </template>
 
 <script>
